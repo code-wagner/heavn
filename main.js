df80d19c9998b9bc7caf4a7c86e648c61b62946d
Wagner Santos <wagner-code@proton.me.com>
2023-04-08 23:22:05 -0300

    Changes to be committed: 	modified:   .gitignore 	modified:   index.html 	modified:   main.js 	new file:   package-lock.json 	new file:   package.json

--- main.js
+++ main.js
@@ -1,70 +1,17 @@
-class Products {
-  constructor(name, message) {
-    this.name = name.toLowerCase();
-    this.message = message;
-  }
-
-  ask() {
-    console.log(this.name + this.message);
-  }
-}
-
-const boards = new Products("Skates", "Decks Models");
-const apparel = new Products("Apparel", "Clothes");
-
-function selectproducts() {
-  let products = prompt("What product do you want?");
-
-  if (products.toLowerCase() === "boards") {
-    alert(boards.message);
-  } else if (products.toLowerCase() === "apparel") {
-    alert(apparel.message);
-  } else {
-    alert("Not Found");
-  }
-}
-
-selectproducts();
-
 const productsArray = [
-  { products: "boards", numberProdutcs: 2 },
-  { products: "apparel", numberProdutcs: 2 },
+  { products: "boards", numberProducts: 2 },
+  { products: "apparel", numberProducts: 2 },
 ];
+
+function selectProducts(criteria) {
+  return productsArray.filter((product) => product[criteria] > 0);
+}
 
 const name = productsArray.map((o) => o.name);
 console.log(productsArray);
 // [ 'decks', 'apparel' ]
 const numberProdutcs = productsArray.map((o) => o.number);
 // [ '2', '2' ]
-
-document.addEventListener("DOMContentLoaded", function () {
-  const emailInput = document.querySelector("#newsletter1");
-  const subscribeBtn = document.querySelector("#subscribeButton");
-
-  // Add event listener to the Subscribe button
-  subscribeBtn.addEventListener("click", function () {
-    const email = emailInput.value;
-
-    // Retrieve existing email list from local storage
-    let emailList = JSON.parse(localStorage.getItem("emailList")) || [];
-
-    // Add new email to the list
-    emailList.push(email);
-
-    // Save updated email list to local storage
-    localStorage.setItem("emailList", JSON.stringify(emailList));
-
-    // Display a success message to the user
-    swal("Nice!", "Thank you for subscribing!", "success");
-  });
-
-  // Initialize the email list from local storage
-  let emailList = JSON.parse(localStorage.getItem("emailList")) || [];
-
-  // Display the email list on the page
-  const emailListElement = document.querySelector("#newsletter1");
-  emailListElement.innerHTML = emailList.join(", ");
-});
 
 const nameProducts = [
   {
@@ -88,6 +35,32 @@ const nameProducts = [
     price: 240.0,
   },
 ];
+
+document.addEventListener("DOMContentLoaded", function () {
+  const emailInput = document.querySelector("#newsletter1");
+  const subscribeBtn = document.querySelector("#subscribeButton");
+
+  subscribeBtn.addEventListener("click", function () {
+    const email = emailInput.value;
+
+    let emailList = JSON.parse(localStorage.getItem("emailList")) || [];
+
+    if (emailList.includes(email)) {
+      swal("Oops!", "This email is already subscribed!", "error");
+    } else {
+      emailList.push(email);
+      localStorage.setItem("emailList", JSON.stringify(emailList));
+      swal("Nice!", "Thank you for subscribing!", "success");
+    }
+  });
+
+  let emailList = JSON.parse(localStorage.getItem("emailList")) || [];
+
+  const emailListElement = document.querySelector("#newsletter1");
+  emailListElement.innerHTML = emailList
+    .map((email) => `<li>${email}</li>`)
+    .join("");
+});
 
 // Pause the video when the user scrolls the page.
 jQuery(window).scroll(function () {
